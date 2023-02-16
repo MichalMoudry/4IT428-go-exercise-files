b633d1c37ff1c62fd184196568525ba73474ebdb
Michal Moudrý <moudry.michal@live.com>
2023-02-16 12:48:11 +0100

    Finished first part of basics

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 4IT428-go-exercise-files
+# 4IT428 - go exercise files
 Files for course 4IT428 - Development of microservices in Go.
---
+++ assinments/basics/01.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func add(a int, b int) int {
+	return a + b
+}
+
+const Test = 50
+
+var c, python, java bool
+
+func main() {
+	var a, b, d = true, false, "Test"
+	fmt.Println("Test", rand.Intn(5), math.Pi)
+	println(add(5, 15))
+	println(c, python, java)
+	println(a, b, d)
+	println(complex(10, 1))
+}
